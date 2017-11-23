3b6d1f80e30d22c948bf6b7878b57c0829e1ccfe
celestin prudhomme <celestin.prudhomme@gmail.com>
2017-11-23 16:58:49 +0100

    css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+
+10.times do
+  cocktail = Cocktail.new(
+    name: Faker::Coffee.blend_name,
+  )
+  cocktail.save
+end
